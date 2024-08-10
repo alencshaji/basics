f50557d3d70c007777ca544d157b08cfe47d9e0f
alencshaji <alencshaji@gmail.com>
2024-08-10 10:21:56 +0530

    updated

--- src/modules/auth/dto/auth.dto.ts
+++ src/modules/auth/dto/auth.dto.ts
@@ -3,6 +3,6 @@ import { ApiProperty } from "@nestjs/swagger";
 export class AuthDto {
   @ApiProperty({ example: 'admin@demo.com', description: 'The user ID' })
   email: string;
-  @ApiProperty({ example: 'password123', description: 'The user password' })
+  @ApiProperty({ example: 'password@123', description: 'The user password' })
   password: string;
 }
